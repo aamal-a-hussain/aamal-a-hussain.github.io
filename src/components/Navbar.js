038491fd41e21d81e57eee6c38b7da684f52ddc4
Cristina <cristina.nogales@automation-consultants.com>
2023-12-27 13:14:55 +0000

    add navbar button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Navbar.css"
+import { MdMenu } from "react-icons/md";
 
 function Navbar() {
+    const [expandNavbar, setExpandNavbar] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        setExpandNavbar(false);
+    }, [location]);
+
     return (
-        <div className="navbar"> 
+        <div className="navbar" id={expandNavbar ? "open" : "close"}>
+            <div>
+                <div className="AH"> Aamal Hussain </div> 
+                
+                <div className="toggleButton">
+                    <button onClick={() => setExpandNavbar((prev) => !prev)}> 
+                        <MdMenu />
+                    </button>
+                </div>
+
+                <div className="links">
+                    <Link to="/"> Home </Link>
+                    <Link to="/research"> Research </Link>
+                    <Link to="/contact"> Contact </Link>
+                </div>
+            </div>
             
-            <div className="toggleButton">
-                <button> </button>
-            </div>
-            <div className="links">
-                <div className="AH"> Aamal Hussain </div>
-                <Link to="/"> Home </Link>
-                <Link to="/research"> Research </Link>
-                <Link to="/contact"> Contact </Link>
-            </div>
         </div>
     );
 }
