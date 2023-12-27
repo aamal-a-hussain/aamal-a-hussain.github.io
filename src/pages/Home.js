af73569edcd12ac760f67ac40a76e7e4b6a49f22
Cristina <cristina.nogales@automation-consultants.com>
2023-12-27 15:50:25 +0000

    add about me in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "../styles/Home.css"
 
 function Home() {
-    return <div>Home</div>;
+    return <div className="home">
+        <div className="about">
+            <h2>About me</h2>
+            <div className="prompt"> 
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining.</p>
+            </div>
+        </div>
+        <div className="Experience">
+            <h2>Experience </h2>
+
+        </div>
+    </div>;
 }
 
 export default Home;
