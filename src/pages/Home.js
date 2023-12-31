4c550bddb8320625e7109ffaef36f719dace39ef
Cristina <cristina.nogales@automation-consultants.com>
2023-12-31 18:09:28 +0000

    made improvements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,24 @@
 import React from "react";
 import "../styles/Home.css"
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css"
 import { IoSchool } from "react-icons/io5";
-import { GiArchiveResearch } from "react-icons/gi";
 import headshot from "../assets/Image.jpeg" 
 
 
 
 function Home() {
     return (
-        <div className="home">
-            <section className="about-wrapper">
-                <div className="about-left">
-                    <img src={headshot} alt="Profile" className="headshot"/>
-                    <p className="name">Aamal Hussain</p>
+        <section className="home">
+            
+            <div className="container home__container">
+
+                <div className="container home__left">
+                    <div className="about-image">
+                        <img src={headshot} alt="About Image" className="img"/>
+                    </div>
+                    <p className="title">Aamal Hussain</p>
                     <p className="curr-title"> Machine Learning Researcher </p>
-                    {/* <p className="curr-location"> PhysicsX </p> */}
-                    <a
-                        href="https://www.physicsx.ai/" 
+                    <a  href="https://www.physicsx.ai/" 
                         target="_blank"
                         rel="noopener noreferrer"
                         className="curr-location">
@@ -26,54 +26,63 @@ function Home() {
                     </a>
                 </div>
 
-                <div className="about-right">
-                    <p className="about-me">About me</p>
-                    <p className="prompt"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining.</p>
+                <div className="container home__right">
+                    <div className="container__about-me">
+                        <p className="title">About me</p>
+                        <p className="about-txt"> I am a passionate researcher in the field of behaviour and decision making, specialising in informed decision making, behaviour change, and the influence of digital communication and interventions. My focus spans a variety of topics, ranging from sexual and reproductive health to the disclosure of mental health issues and climate change. In essence, anything related to behaviour and decision making, as well as its support, captures my interest. </p>
+                    </div>
+
+                    <div className="container home__right-bottom">
+                        <div className="container__interests">
+                            <p className="title-2">  Interests </p>
+                            <ul className="bullet-points">
+                                <li>Game Theory</li>
+                                <li>Control</li>
+                                <li>Dynamical Systems</li>
+                                <li>Dynamical Systems</li>
+                            </ul>
+                        </div>
+
+                        <div className="container__experience">
+                            <p className="title-2"> Education </p>
+                            {/* <div className="education-list"> */}
+                            <div className="education-item">
+                                <IoSchool size={45} className="education-icon"/>
+                                <div>
+                                    <p className="education-title"> PhD in Reinforcement Learning and Game Theory</p>
+                                    <p className="education-location">Imperial College London, 2020-2024</p>
+                                </div>
+                            </div>
+
+                            <div className="education-item">
+                                <IoSchool size={45} className="education-icon"/>
+                                <div>
+                                    <p className="education-title">MSc in Robotics and Computation</p>
+                                    <p className="education-location">University College London (UCL), 2018-2019</p>
+                                </div>
+                            </div>
+
+                            <div className="education-item">
+                                <IoSchool size={45} className="education-icon"/>
+                                <div>
+                                    <p className="education-title">BS Physics</p>
+                                    <p className="education-location">Imperial College London, 2015-2018</p>
+                                </div>
+                            </div>
+
+                            {/* <div> */}
+                        </div>
+                    </div>
                 </div>
-            </section>
 
-            <section className="experience">
-                <h2>Experience & Education </h2>
-                <VerticalTimeline lineColor="#0b1b43">
-                    <VerticalTimelineElement 
-                        className="vertical-timeline-element--education"
-                        date="2018-2019"
-                        iconStyle={{ background: "#0b1b43", color:  "	#FFFFFF"}}
-                        icon={<GiArchiveResearch />}>
-                        <h3 className="vertical-timeline-element-title">
-                            Imperial College London
-                        </h3>
-                        <h4>Doctoral Student</h4>
-                        <p>Studying the Dynamical Systems Theory of Learning and Control of Multi-Agent Systems. In particular, we are analysing the Stability of Learning in Games with a large population of independent learners. We seek also to understand problems of formation control and swarm dynamics when modelled as an network game.</p>
-                        <p>Funded under the Centre for Doctoral Training in Safe and Trusted AI, a collaboration between Imperial College London and King's College London.</p>
-                    </VerticalTimelineElement>
+            </div>
 
-                    <VerticalTimelineElement 
-                        className="vertical-timeline-element--education"
-                        date="2018-2019"
-                        iconStyle={{ background: "#0b1b43", color:  "#FFFFFF"}}
-                        icon={<IoSchool />}>
-                        <h3 className="vertical-timeline-element-title">
-                            University College London (UCL)
-                        </h3>
-                        <p>Master of Science - MS, Robotics and Computation</p>
-                    </VerticalTimelineElement>
+            <div className="container  container__tech-stack">
+                <p className="title-2">Tech Stack</p>
+                <p className="title-2">Skills</p>
+            </div>
 
-                    <VerticalTimelineElement 
-                        className="vertical-timeline-element--education"
-                        date="2015-2018"
-                        iconStyle={{ background: "#0b1b43", color:  "#FFFFFF"}}
-                        icon={<IoSchool />}>
-                        <h3 className="vertical-timeline-element-title">
-                            Imperial College London
-                        </h3>
-                        <p>Bachelor of Science - BS, Physics</p>
-                    </VerticalTimelineElement>
-
-                </VerticalTimeline>
-
-            </section>
-        </div>
+        </section>
     );
 }
 
