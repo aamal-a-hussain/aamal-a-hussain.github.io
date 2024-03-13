60f7e99884403e2308f423cd3dd42b571b806fff
Cristina <cristina.nogales@automation-consultants.com>
2024-03-13 18:02:41 +0000

    reformat talk images

--- src/components/TalkItem.js
+++ src/components/TalkItem.js
@@ -3,7 +3,10 @@ import React from "react";
 function TalkItem({description, location, title, image_name}) {
     return (
     <div className="talk-item">
-        <img src={require(`../assets/${image_name}.png`)} className="talk-img"/>
+        <div className="talk-img">
+            <img src={require(`../assets/${image_name}.png`)}/>
+        </div>
+        
         <div className="talk-details">
             <p className="talk-title"> {title} </p>
             <p className="talk-location"> {location} </p>
